8b29c1ba9fd04e87d44c30b7a166d5e32da8f39d
Mayank Mani Tripathi <45062501+yesmayank@users.noreply.github.com>
2023-10-23 14:58:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,86 @@
+class logger{
+    method = "log";
+    showDateTime = false;
+    color = {
+        black: "\u001b[30m",
+        red: "\u001b[31m",
+        green: "\u001b[32m",
+        yellow: "\u001b[33m",
+        blue: "\u001b[34m",
+        magenta: "\u001b[35m",
+        cyan: "\u001b[36m",
+        white: "\u001b[37m",
+        alert: "\u001b[31m",
+        warn: "\u001b[33m",
+        log: "\u001b[37m"
+    }
+
+    constructor(...options){
+        if(options[0] == true) {
+            this.showDateTime = true;
+        }
+        return;
+        if(method == "warn"){
+        
+            this.method = "warn";
+        } else if(method == "alert"){
+        
+            this.method = "alert";
+        } else {
+            
+            this.method = "log";
+        }
+    }
+
+    getCurrentDateTime(){
+        let currentdate = new Date(); 
+        let datetime = "" + currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+        return datetime;
+    }
+
+    getColorCode(code) {
+        let color = "\x1b[36m";
+
+        try{
+            color = this.color[code];
+        } catch(e){
+            color = this.color("white");
+        }
+
+        return color;
+    }
+
+    getLogFormat(data){
+        let text = data.split(" ")
+        
+        let color = text[0];
+        color = color.replace("@", "");
+
+        text = text.slice(1);
+        text = text.join(" ");
+
+        color = this.getColorCode(color);
+        return Array(color, text);
+    }
+
+    log(...data) {
+        data = data.join(" ");
+        let txt = this.getLogFormat(data);
+        let format = txt[0]
+        data = txt[1]
+        if(this.showDateTime){
+            let consoleData =  format  + "[ " + this.getCurrentDateTime() + " ]  -  " + data + format;
+            console.log(consoleData);
+        } else {
+            
+            console.log(data);
+        }
+    }
+}
+
+module.exports = logger;
